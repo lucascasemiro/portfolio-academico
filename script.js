a0cb396a70d50dba03b920cbe01287c6c3634d82
Lucas Casemiro <141570809+lucascasemiro@users.noreply.github.com>
2025-04-24 19:06:19 -0300

    primeira versão da feature de preview

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-lucide.createIcons()
+document.addEventListener("DOMContentLoaded", () => {
+    lucide.createIcons();
+  });
+
+  function openPreview(){
+    const overlay = document.getElementById("overlay");
+    overlay.style.display = "flex";
+    document.body.classList.add("noscroll");
+  }
+  function closePreview(){
+    const overlay = document.getElementById("overlay");
+    const esc = document.getElementById("iframe-esc");
+    overlay.style.display = "none";
+    document.body.classList.remove("noscroll");
+  }
